e71c9417da45d8d5daac329c7a8f960f92cd7b12
Rem Smiers <remq@live.nl>
2020-03-30 09:02:42 +0200

    Initial version

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,54 @@
 import express from "express";
+import { createServer } from "http";
 import socketIO from "socket.io";
+import { v4 } from "uuid";
 
 const port = process.env.PORT || 8080;
 const app = express();
-const io = socketIO();
+const http = createServer(app);
+const io = socketIO(http);
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello world</h1>");
+app.use(express.json());
+
+interface Player {
+  id: string;
+  name: string;
+}
+
+interface Room {
+  code: string;
+  players: Player[];
+}
+
+const rooms: { [key: string]: Room } = {};
+
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+const generateRoomCode = () => {
+  let roomCode = "";
+  for (let i = 0; i < 4; i++) {
+    roomCode +=
+      alphabet[Math.floor(Math.random() * Math.floor(alphabet.length))];
+  }
+  return roomCode;
+};
+
+app.post("/room", (req, res) => {
+  const { name } = req.body as { name: string };
+  let code = generateRoomCode();
+  while (typeof rooms[code] !== "undefined") {
+    generateRoomCode();
+  }
+  rooms[code] = { code, players: [{ id: v4(), name }] };
+  res.json(rooms[code]);
 });
 
 io.on("connection", socket => {
   console.log("a user connected!");
+  socket.on("disconnect", () => {
+    console.log("a user disconnected!");
+  });
 });
 
-app.listen(port, () => {
+http.listen(port, () => {
   console.log(`Let's play some Flunkyball on port ${port}! 🍺`);
 });
